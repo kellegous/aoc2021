b9827f1a4830b88894c64efdd31d73d597f85d4e
Kelly Norton <kellegous@gmail.com>
2021-12-09 10:45:39 -0500

    day3 refactor

--- src/day3/main.rs
+++ src/day3/main.rs
@@ -4,49 +4,38 @@ use std::io::{BufRead, BufReader, Read};
 
 #[derive(Debug)]
 struct Report {
-	g: u16,
-	e: u16,
+	n: usize,
+	vals: Vec<u16>,
 }
 
 impl Report {
 	fn from_reader<R: Read>(r: R) -> Result<Report, Box<dyn Error>> {
 		let r = BufReader::new(r);
-		// num_ones will hold the count of ones for each position
-		let mut num_ones = Vec::new();
+
+		let mut vals = Vec::new();
 		let mut n = 0;
 		for line in r.lines() {
 			let line = line?;
-			if num_ones.is_empty() {
-				num_ones.resize(line.len(), 0);
-			}
-
-			let val = u16::from_str_radix(&line, 2)?;
-			n += 1;
-
-			for i in 0..num_ones.len() {
-				let m = 1 << i;
-				num_ones[i] += ((val & m) > 0) as usize;
-			}
+			n = line.len();
+			vals.push(u16::from_str_radix(&line, 2)?);
 		}
 
-		let mut g = 0u16;
-		let lim = n >> 1;
-		for (i, &v) in num_ones.iter().enumerate() {
-			g |= ((v >= lim) as u16) << i;
+		Ok(Report { n, vals })
+	}
+
+	fn part1(&self) -> usize {
+		let mut g = 0;
+		let lim = self.vals.len() >> 1;
+		for i in 0..self.n {
+			let m = 1 << i;
+			let mut c = 0;
+			for val in &self.vals {
+				c += ((val & m) > 0) as usize;
+			}
+			g |= ((c >= lim) as usize) << i;
 		}
 
-		Ok(Report {
-			g,
-			e: !g & ((1 << num_ones.len()) - 1),
-		})
-	}
-
-	fn epsilon(&self) -> usize {
-		self.e as usize
-	}
-
-	fn gamma(&self) -> usize {
-		self.g as usize
+		g * (!g & ((1 << self.n) - 1))
 	}
 }
 
@@ -63,7 +52,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 		matches.value_of("input").unwrap_or("data/day3/input.txt"),
 	)?)?;
 
-	println!("Part #1: {}", report.gamma() * report.epsilon());
+	println!("Part #1: {}", report.part1());
 
 	Ok(())
 }
