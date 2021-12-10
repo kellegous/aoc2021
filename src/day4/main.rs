da039c0de0fa90c3224e1cfad41b57b130cd4343
Kelly Norton <kellegous@gmail.com>
2021-12-10 15:28:50 -0500

    day4.2 ... also separated play state from input state

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read};
@@ -58,7 +58,7 @@ impl Input {
 				for j in 0..5 {
 					let offset = j * 3;
 					let v = line[offset..offset + 2].trim().parse::<u8>()?;
-					card.tiles[i * 5 + j] = Tile::new(v);
+					card.set(i, j, v);
 				}
 
 				line.clear();
@@ -69,103 +69,20 @@ impl Input {
 
 		Ok(Input { draws, cards })
 	}
-
-	fn play(&mut self) -> Option<(u8, &Card)> {
-		let mut idx: HashMap<u8, Vec<(usize, usize)>> = HashMap::new();
-		for (i, card) in self.cards.iter().enumerate() {
-			for (j, tile) in card.tiles.iter().enumerate() {
-				idx.entry(tile.num).or_default().push((i, j));
-			}
-		}
-
-		for &draw in self.draws.iter() {
-			for (i, j) in idx.entry(draw).or_default().iter() {
-				if self.cards[*i].mark(*j) {
-					return Some((draw, &self.cards[*i]));
-				}
-			}
-		}
-
-		None
-	}
 }
 
 #[derive(Debug)]
 struct Card {
-	tiles: [Tile; 25],
+	tiles: [u8; 25],
 }
 
 impl Card {
 	fn empty() -> Card {
-		Card {
-			tiles: [Tile::default(); 25],
-		}
+		Card { tiles: [0; 25] }
 	}
 
-	fn has_winning_row(&self, idx: usize) -> bool {
-		let ix = idx / 5;
-		(ix..ix + 5).find(|&i| !self.tiles[i].has_mark).is_none()
-	}
-
-	fn has_winning_col(&self, idx: usize) -> bool {
-		let ix = idx % 5;
-		(0..5).find(|&i| !self.tiles[i * 5 + ix].has_mark).is_none()
-	}
-
-	fn mark(&mut self, idx: usize) -> bool {
-		self.tiles[idx].has_mark = true;
-		self.has_winning_col(idx) || self.has_winning_row(idx)
-	}
-
-	fn sum_unmarked(&self) -> usize {
-		self.tiles
-			.iter()
-			.filter(|t| !t.has_mark)
-			.map(|t| t.num as usize)
-			.sum()
-	}
-}
-
-impl std::fmt::Display for Card {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		for row in self.tiles.chunks(5) {
-			let s = row
-				.iter()
-				.map(|t| {
-					if t.has_mark {
-						format!("[{:2}]", t.num)
-					} else {
-						format!(" {:2} ", t.num)
-					}
-				})
-				.collect::<Vec<String>>()
-				.join(" ");
-			writeln!(f, "{}", s)?;
-		}
-		Ok(())
-	}
-}
-
-// TODO(knorton): You know what's stupid? Mixing game state with game declaration. That's
-// what's stupid. You need to undo that. It's as clear as 1 + 1 = 11.
-#[derive(Clone, Copy, Debug)]
-struct Tile {
-	num: u8,
-	has_mark: bool,
-}
-
-impl Tile {
-	fn new(num: u8) -> Tile {
-		Tile {
-			num,
-			has_mark: false,
-		}
-	}
-}
-
-impl Default for Tile {
-	fn default() -> Self {
-		Tile::new(0)
+	fn set(&mut self, i: usize, j: usize, num: u8) {
+		self.tiles[i * 5 + j] = num;
 	}
 }
 
@@ -194,8 +111,119 @@ impl From<&str> for PlayError {
 
 impl Error for PlayError {}
 
-fn part1(input: &mut Input) -> Result<usize, Box<dyn Error>> {
-	match input.play() {
+struct Game<'a> {
+	draws: &'a [u8],
+	cards: Vec<CardState<'a>>,
+}
+
+impl<'a> Game<'a> {
+	fn from_input(input: &'a Input) -> Game {
+		let cards = input
+			.cards
+			.iter()
+			.map(|c| CardState::new(c))
+			.collect::<Vec<CardState>>();
+		Game {
+			draws: &input.draws,
+			cards,
+		}
+	}
+
+	fn play(&mut self) -> Option<(u8, &CardState)> {
+		let mut idx: HashMap<u8, Vec<(usize, usize)>> = HashMap::new();
+		for (i, card) in self.cards.iter().enumerate() {
+			for (j, tile) in card.tiles().iter().enumerate() {
+				idx.entry(*tile).or_default().push((i, j));
+			}
+		}
+
+		for &draw in self.draws {
+			for (i, j) in idx.entry(draw).or_default().iter() {
+				if self.cards[*i].mark(*j) {
+					return Some((draw, &self.cards[*i]));
+				}
+			}
+		}
+		None
+	}
+
+	fn play_all(&mut self) -> Option<(u8, &CardState)> {
+		let mut idx: HashMap<u8, Vec<(usize, usize)>> = HashMap::new();
+		let mut has_won = HashSet::new();
+
+		for (i, card) in self.cards.iter().enumerate() {
+			for (j, tile) in card.tiles().iter().enumerate() {
+				idx.entry(*tile).or_default().push((i, j));
+			}
+		}
+
+		for &draw in self.draws {
+			for (i, j) in idx.entry(draw).or_default().iter() {
+				if self.cards[*i].mark(*j) {
+					has_won.insert(*i);
+					if has_won.len() == self.cards.len() {
+						return Some((draw, &self.cards[*i]));
+					}
+				}
+			}
+		}
+		None
+	}
+}
+
+struct CardState<'a> {
+	card: &'a Card,
+	marks: [bool; 25],
+}
+
+impl<'a> CardState<'a> {
+	fn new(card: &'a Card) -> CardState {
+		CardState {
+			card: card,
+			marks: [false; 25],
+		}
+	}
+
+	fn tiles(&self) -> &[u8] {
+		&self.card.tiles
+	}
+
+	fn is_winning_row(&self, idx: usize) -> bool {
+		let ix = (idx / 5) * 5;
+		(ix..ix + 5).find(|&i| !self.marks[i]).is_none()
+	}
+
+	fn is_winning_col(&self, idx: usize) -> bool {
+		let ix = idx % 5;
+		(0..5).find(|&i| !self.marks[i * 5 + ix]).is_none()
+	}
+
+	fn mark(&mut self, idx: usize) -> bool {
+		self.marks[idx] = true;
+		self.is_winning_row(idx) || self.is_winning_col(idx)
+	}
+
+	fn sum_unmarked(&self) -> usize {
+		self.marks
+			.iter()
+			.enumerate()
+			.filter(|(_, &marked)| !marked)
+			.map(|(i, _)| self.card.tiles[i] as usize)
+			.sum()
+	}
+}
+
+fn part1(input: &Input) -> Result<usize, Box<dyn Error>> {
+	let mut game = Game::from_input(input);
+	match game.play() {
+		Some((draw, card)) => Ok(draw as usize * card.sum_unmarked()),
+		None => Err(Box::new(PlayError::from("no winner found"))),
+	}
+}
+
+fn part2(input: &Input) -> Result<usize, Box<dyn Error>> {
+	let mut game = Game::from_input(input);
+	match game.play_all() {
 		Some((draw, card)) => Ok(draw as usize * card.sum_unmarked()),
 		None => Err(Box::new(PlayError::from("no winner found"))),
 	}
@@ -210,10 +238,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 		)
 		.get_matches();
 
-	let mut input = Input::from_reader(File::open(
+	let input = Input::from_reader(File::open(
 		matches.value_of("input").unwrap_or("data/day4/input.txt"),
 	)?)?;
 
-	println!("Part 1: {}", part1(&mut input)?);
+	println!("Part 1: {}", part1(&input)?);
+	println!("Part 2: {}", part2(&input)?);
 	Ok(())
 }
